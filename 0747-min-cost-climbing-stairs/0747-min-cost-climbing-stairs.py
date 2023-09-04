d4a6d7707ff6414935f8bdff23e848aea73cfb0c
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2023-09-05 06:24:31 +0900

    Time: 49 ms (98.69%), Space: 18.7 MB (10.84%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -2,6 +2,7 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         memo = {}
         N = len(cost)
+        
         def dp(i):
             if i in memo:
                 pass
@@ -9,5 +10,7 @@ class Solution:
                 memo[i] = 0
             else:
                 memo[i] = cost[i] + min(dp(i+1), dp(i+2))
+
             return memo[i]
+        
         return min(dp(0), dp(1))
