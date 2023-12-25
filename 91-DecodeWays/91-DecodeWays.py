63f55607eee16fc571a0d2682f275d43b263a9a4
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2023-12-25 11:21:40 -0600

    Create file

---
+++ 91-DecodeWays/91-DecodeWays.py
@@ -0,0 +1,17 @@
+                for i in range(1, 27):
+                    
+                    if len(cur) == 1 and s[0] == cur:
+                        res += dp(s[1:])
+            else:
+                res = 0
+                    cur = str(i)
+                memo[s] = 1
+                pass
+            elif s == "":
+        def dp(s):
+            if s in memo:
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+
+"
