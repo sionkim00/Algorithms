ff4d9f4031c9c64681733760a46a86b5c50ce255
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-01-18 23:48:42 -0600

    Create file

---
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.py
@@ -0,0 +1,16 @@
+            elif i == 0:
+                for k in range(N):
+            elif j < 0 or j >= N:
+                memo[key] = float("inf")
+                memo[key] = float("inf")
+            elif i == N:
+                memo[key] = 0
+            if key in memo:
+                pass
+            key = (i,j)
+        memo = defaultdict()
+        def dp(i, j):
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        N = len(matrix)
+[
