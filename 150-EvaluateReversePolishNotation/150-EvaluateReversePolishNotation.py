62bbd42641d3542dab87f211bf6cb07366c5c532
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-01-29 18:40:45 -0600

    Create file

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
@@ -0,0 +1,9 @@
+        s = []
+
+        for token in tokens:
+            if token == "+":
+                second = s.pop()
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+                first = s.pop()
+[
