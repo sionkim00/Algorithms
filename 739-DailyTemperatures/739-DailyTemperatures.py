ed19e987d27733c2eef9eaa2416857950d9d0133
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-01-31 10:47:28 -0600

    Create file

---
+++ 739-DailyTemperatures/739-DailyTemperatures.py
@@ -0,0 +1,8 @@
+            while minHeap and minHeap[0][0] < temperatures[i]:
+        for i in range(N):
+
+        res = [0] * N
+        N = len(temperatures)
+        heapq.heapify(minHeap)
+        minHeap = []
+[
