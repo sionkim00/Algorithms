b9cab50505052e0da42c4457811f5c345327c455
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-02-01 07:19:06 -0600

    Create file

---
+++ 2966-DivideArrayIntoArraysWithMaxDifference/2966-DivideArrayIntoArraysWithMaxDifference.py
@@ -0,0 +1,10 @@
+            if curMax - curMin > k:
+                return []
+        
+
+            curMax = max(nums[i*3:i*3+3])
+            curMin = min(nums[i*3:i*3+3])
+        for i in range(len(nums) // 3):
+        nums.sort()
+        res = []
+[
