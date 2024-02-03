8a79c6df078c442916cdcdfce772b302eace5db4
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-02-02 21:19:47 -0600

    Create file

---
+++ 1043-PartitionArrayforMaximumSum/1043-PartitionArrayforMaximumSum.py
@@ -0,0 +1,15 @@
+                cur_max = 0
+                res = 0
+                for j in range(i, min(len(arr), i+k)):
+            else:
+                memo[i] = 0
+            elif i >= N:
+                pass
+        def dp(i):
+            if i in memo:
+        memo = defaultdict()
+
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        N = len(arr)
+[
