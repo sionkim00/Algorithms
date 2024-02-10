fbb3b3a1704b3e41ca978f6060034df61ee224a7
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-02-09 23:43:08 -0600

    Create file

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -0,0 +1,16 @@
+                memo[key] = True
+            else:
+                memo[key] = False
+            return memo[key]
+        
+        cnt = 0
+            elif s[start] == s[end] and dp(start+1, end-1):
+            elif start >= end:
+                memo[key] = True
+            if key in memo:
+                pass
+        def dp(start, end):
+            key = (start,end)
+        memo = defaultdict()
+
+"
