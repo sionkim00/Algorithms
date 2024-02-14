8d0e583578ec8088dfc376eb73dda80016779f05
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-02-13 21:47:04 -0600

    Create file

---
+++ 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.py
@@ -0,0 +1,7 @@
+                neg.append(num)
+        for i in range(len(nums) // 2):
+        res = []
+            res.append(pos.popleft())
+            res.append(neg.popleft())
+        return res
+[
