fab387b7c26f4a6b547ff03eccad9c58c9d861b4
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-02-22 00:20:16 -0600

    Create file

---
+++ 997-FindtheTownJudge/997-FindtheTownJudge.py
@@ -0,0 +1,16 @@
+
+        for a, b in trust:
+            kvp[b].add(a)
+
+        for key in kvp:
+            if not(b in kvp):
+                kvp[b] = set()
+        trustsSomeone = defaultdict(bool)
+            trustsSomeone[a] = True
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        kvp = defaultdict()
+class Solution:
+
+        if n == 1:
+            return 1
+2
