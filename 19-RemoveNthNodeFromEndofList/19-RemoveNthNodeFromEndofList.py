03fdf0f9d1a2fef579a604e576ca09ac0755121d
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-03-02 20:51:46 -0600

    Create file

---
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -0,0 +1,12 @@
+
+        for i in range(n+1):
+            fast = fast.next
+        
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
+
+        return dummy.next
+[
