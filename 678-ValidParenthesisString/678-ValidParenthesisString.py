8f0f6c3b05f3beba9543c2b7fda7a5faac785f06
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-04-06 22:30:49 -0500

    Create file

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.py
@@ -0,0 +1,14 @@
+                lMin, lMax = lMin + 1, lMax + 1
+            elif c == ")":
+                lMin, lMax = lMin - 1, lMax - 1
+            else:
+                lMin, lMax = lMin - 1, lMax + 1
+            if lMax < 0:
+                return False
+            if lMin < 0:
+                lMin = 0
+            if c == "(":
+        for c in s:
+        return lMin == 0
+            
+"
