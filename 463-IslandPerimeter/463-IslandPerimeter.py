bdd8fcca6339bc0bd437080aa9278eccccf82322
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-04-18 07:21:33 -0500

    Create file

---
+++ 463-IslandPerimeter/463-IslandPerimeter.py
@@ -0,0 +1,11 @@
+
+                    if min(newI, newJ) < 0 or newI >= N 
+or newJ >= M:
+                        ans += 1
+                    elif grid[newI][newJ] == 0:
+                    newJ = j + path[1]
+                        print(newI, newJ)
+                    newI = i + path[0]
+                for path in paths:
+
+[
