dc20bc21182c5d19fd0f0f9679242046c1dd672a
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-04-21 09:16:32 -0500

    Create file

---
+++ 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.py
@@ -0,0 +1,21 @@
+        def dfs(node, visited):
+            for neighbor in graph[node]:
+
+            if node == destination:
+                return True
+
+            graph[b].append(a)
+            graph[a].append(b)
+        for a, b in edges:
+
+        graph = defaultdict(list)
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+class Solution:
+            visited.add(node)
+
+                if neighbor not in visited:
+                    if dfs(neighbor, visited):
+                        return True
+            return False
+        return dfs(source, set())
+3
