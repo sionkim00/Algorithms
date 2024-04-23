a4f2d7c15fd45e02f232472f61776ebbe5e50056
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-04-22 23:24:34 -0500

    Create file

---
+++ 310-MinimumHeightTrees/310-MinimumHeightTrees.py
@@ -0,0 +1,9 @@
+        for src, neighbors in adj.items():
+            if len(neighbors) == 1:
+        leaves = deque()
+        edge_cnt = {}
+
+            adj[b].append(a)
+                leaves.append(src)
+            edge_cnt[src] = len(neighbors)
+4
