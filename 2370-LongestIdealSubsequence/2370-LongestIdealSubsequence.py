d97c8515ee58bdab6d9be41a72f186e68d73f027
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-04-25 07:57:11 -0500

    Update file 4/25/2024, 7:57:11 AM

--- 2370-LongestIdealSubsequence/2370-LongestIdealSubsequence.py
+++ 2370-LongestIdealSubsequence/2370-LongestIdealSubsequence.py
@@ -1,14 +1,11 @@
-            maxi = float("-inf") 
+            maxVal = float("-inf")
 
-            left = max((idx - k), 0)
-            right = min((idx + k), 26)
+            left = max(0, charIdx - k)
+            right = min(26, charIdx + k)
 
             for j in range(left, right + 1):
-                maxi = max(maxi, dp[j])
+                maxVal = max(maxVal, dp[j])
             
-            dp[idx] = maxi + 1
-            idx = ord(cur) - ord('a')
-            cur = s[i]
-        for i in range(N-1, -1, -1):
-
+            dp[charIdx] = maxVal + 1
+        return max(dp)
 "
