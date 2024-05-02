faed4c3354c4ef0534faaa38969823e8a50907bc
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-05-02 09:15:03 -0500

    Create file

---
+++ 2000-ReversePrefixofWord/2000-ReversePrefixofWord.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: 
+str) -> str:
+        i = word.find(ch)
+        if i != -1:
+            return word[:i+1][::-1] + word[i
++1:]
+        return word
+"
