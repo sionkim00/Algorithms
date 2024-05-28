9fd8c793a605de256f074339610fa8caa2272b69
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-05-27 20:13:23 -0500

    Create file

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -0,0 +1,11 @@
+        for key in counter:
+            heapq.heappush(heap, [-1*counter[key], 
+        ans = []
+            cur = heapq.heappop(heap)
+        for i in range(k):
+            ans.append(cur[1])
+        heap = []
+        heapq.heapify(heap)
+key])
+        return ans
+[
