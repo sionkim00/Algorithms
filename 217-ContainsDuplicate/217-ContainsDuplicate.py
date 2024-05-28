97dbfebb58208019c66fe7e3574cc601fd93c9e9
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-05-27 19:54:32 -0500

    Create file

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -0,0 +1,10 @@
+    def containsDuplicate(self, nums: List[int]) -> 
+bool:
+        s = set()
+
+        for num in nums:
+            if num in s:
+                return True
+            s.add(num)
+        return False
+[
