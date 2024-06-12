24162e7f81877115c3a040760889fc626cec643c
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-06-12 07:14:50 -0500

    Create file

---
+++ 15-3Sum/15-3Sum.py
@@ -0,0 +1,12 @@
+                if threeSum > 0:
++ nums[i]
+            l, r = i + 1, N - 1
+            while l < r:
+                threeSum = nums[l] + nums[r] 
+                continue
+                
+            if i > 0 and nums[i] == nums[i-1]:
+
+        for i in range(N):
+        res = []
+[
