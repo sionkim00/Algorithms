94a1448322ebde73de7f049160343ed57aff48a9
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-07-06 16:28:41 -0500

    Create file

---
+++ 2582-PassthePillow/2582-PassthePillow.py
@@ -0,0 +1,13 @@
+                direction = 1
+            if cur == 1 and i != 0:
+                direction = 0
+
+            if direction == 0:
+                cur += 1
+            if cur == n:
+        direction = 0 # 0 = right 1 = left
+        for i in range(time):
+        cur = 1
+    def passThePillow(self, n: int, time: int) -> int:
+class Solution:
+4
