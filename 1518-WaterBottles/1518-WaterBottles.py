23a086f644aa2890a0300b1e4b2f8e013a7419d9
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-07-07 15:49:10 -0500

    Create file

---
+++ 1518-WaterBottles/1518-WaterBottles.py
@@ -0,0 +1,8 @@
+            numEmptyBottles += numFullBottles
+            ans += numFullBottles
+            numFullBottles = numEmptyBottles // 
+numExchange
+            numEmptyBottles %= numExchange
+        return ans
+
+9
