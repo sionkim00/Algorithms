e0a2e758202b94a493dbae8a210e6656f8c97dc4
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-08-01 22:08:29 -0500

    Create file

---
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return [*nums, *nums]
+[
+[1,2,1]
+[1,3,2,1]
+[1,2,1,1,2,1]
+[1,3,2,1,1,3,2,1]
+[1,2,1,1,2,1]
+[1,3,2,1,1,3,2,1]
