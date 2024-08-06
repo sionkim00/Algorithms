dee540654e730c02e8189f05bbfe0b7c8f7e5e3e
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-08-05 20:50:12 -0500

    Create file

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -0,0 +1,28 @@
+            else:
+                head.next = list1
+                list1 = list1.next
+        while list1:
+            head.next = list1
+                head = head.next
+                head = head.next
+                list2 = list2.next
+                head.next = list2
+        while list1 and list2:
+            if list1.val > list2.val:
+
+        newHead = ListNode(0)
+        head = newHead
+            head = head.next
+[
+[1,2,4]
+[1,3,4]
+[]
+[]
+[]
+[0]
+[1,1,2,3,4,4]
+[]
+[0]
+[1,1,2,3,4,4]
+[]
+[0]
