a0f8ef4faea9d5471bdc033edd150287a93ea95e
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-08-14 08:05:13 -0500

    Create file

---
+++ 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.py
@@ -0,0 +1,20 @@
+            if guess(mid) == -1:
+                r = mid - 1
+            elif guess(mid) == 1:
+                l = mid + 1
+            else:
+                return mid
+                
+1
+10
+6
+1
+1
+2
+1
+6
+1
+1
+6
+1
+1
