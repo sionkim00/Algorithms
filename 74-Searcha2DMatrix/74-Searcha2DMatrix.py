f61179ebaa07b07f124bb86ddf013a5dd977bd71
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-08-14 07:25:57 -0500

    Create file

---
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -0,0 +1,18 @@
+:
+            if matrix[mid][0] <= target <= matrix[mid][-1]
+            mid = (top + bot) // 2
+        while top <= bot:
+        top, bot = 0, N - 1
+                break
+            elif matrix[mid][0] > target:
+                bot = mid - 1
+            else:
+[
+[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+3
+[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+13
+true
+false
+true
+false
