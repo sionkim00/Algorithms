3fe970b3fad155bd69a085f0ca9bc84d5101d775
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-09-10 23:30:28 -0500

    Update file 9/10/2024, 11:30:28 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,10 +1,19 @@
-    def containsDuplicate(self, nums: List[int]) -> 
-bool:
-        s = set()
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        kvp = defaultdict(bool)
 
         for num in nums:
-            if num in s:
+            if num in kvp:
                 return True
-            s.add(num)
+            kvp[num] = True
         return False
 [
+[1,2,3,1]
+[1,2,3,4]
+[1,1,1,3,3,4,3,2,4,2]
+true
+false
+true
+true
+false
+true
